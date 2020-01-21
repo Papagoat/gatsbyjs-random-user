fcc62bfb6edcfc2c45bcdc92805bfbc4782d8c75
I001962 <i001962@gmail.com>
2020-01-20 20:15:15 -0800

    add required fields for street name and number

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ export const query = graphql`
             thumbnail
           }
           location {
-            street
+            street{number name}
             city
             state
           }
